ce3a1167daabefeb24a0a639d2f922d061d0bd46
brash99 <brash@jlab.org>
2025-05-26 23:02:07 -0400

    updates

---
+++ setup_cdet_pd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\n"
+echo " Welcome to CDET analysis framework! " 
+echo -e "\n"
+
+#module purge
+#module load analyzer/1.7.12-sbs6
+
+
+export SBS_REPLAY=/Users/brash/CDET_replay/git-repo/sbs_devel/SBS-replay
+export DB_DIR=/Users/brash/CDET_replay/git-repo/sbs_devel/SBS-replay/DB
+export DATA_DIR=/Users/brash/sbs/data
+#export DATA_DIR=/cache/halla/sbs/GEnRP/raw
+export OUT_DIR=/Users/brash/sbs/Rootfiles
+
